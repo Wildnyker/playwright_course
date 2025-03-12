de8ea881a5ff78196eab61cca41be2f8d1b1fd4e
Wildnyker <sgrynchuk@outlook.com>
2025-03-13 00:19:58 +0100

    soma additons to lesson 8

--- lessons/lesson8.js
+++ lessons/lesson8.js
@@ -55,3 +55,19 @@ printAge(10)
 import * as helper from '../helpers/printHelper.js'
 
 helper.printAge(11)
+
+
+let familySize = 2
+let plannedDistanceToDrive = 100
+
+function recommendedCar(familySize, plannedDistanceToDrive) {
+    if (familySize <= 4 && plannedDistanceToDrive < 200) {
+        return('Tesla')
+    } else if (familySize <= 4 && plannedDistanceToDrive >= 200){
+        return('Toyota Camry')
+    } else if (familySize > 4){
+        return('Minivan')
+    }
+}
+
+console.log(recommendedCar(familySize, plannedDistanceToDrive));
